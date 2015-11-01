0c92cc7f808936328f71e4d86fed710ce5a876ad
Sandy Maguire <sandy@sandymaguire.me>
2015-11-01 01:10:29 -0700

    better error messages

--- src/Experimental.hs
+++ src/Experimental.hs
@@ -26,22 +26,21 @@ newtype JurisdictionT s r m a =
 
 instance (Applicative m, Monad m) => Applicative (JurisdictionT s r m) where
     pure x = JurisdictionT $ \l s -> pure (x, s, l)
-    (<*>) = ap
+    (<*>)  = ap
 
 instance (Applicative m, Monad m) => Monad (JurisdictionT s r m) where
-    return = pure
+    return   = pure
     ac >>= k = JurisdictionT $ \l s -> do
         (x, st', l') <- runJurisdictionT' ac l s
         runJurisdictionT' (k x) l' st'
 
 instance (Applicative m, Monad m) => MonadReader s (JurisdictionT s r m) where
-    ask = JurisdictionT $ \l s -> return (s, s, l)
-    local = error "fuck"
+    ask   = JurisdictionT $ \l s -> return (s, s, l)
+    local = error "Can't call local on a Jurisdiction"
 
 instance (Applicative m, Monad m) => MonadState r (JurisdictionT s r m) where
-    get = JurisdictionT $ \l s -> return (view l s, s, l)
+    get   = JurisdictionT $ \l s -> return (view l s, s, l)
     put v = JurisdictionT $ \l s -> return ((), set l v s, l)
-    state = error "fuck"
 
 instance MonadTrans (JurisdictionT s r) where
     lift x = JurisdictionT $ \l s -> x >>= (\a -> return (a, s, l))
@@ -61,9 +60,4 @@ restrict :: (Monad m) => RLens r' r -> JurisdictionT s r' m a -> JurisdictionT s
 restrict l' m = JurisdictionT $ \l s -> do
     (a, s', _) <- runJurisdictionT' m (l . l') s
     return (a, s', l)
-
--- restrict
--- inq|uire
--- imp|ose
-
 
